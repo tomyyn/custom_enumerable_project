e0d5b4dbdd303e19d5bb6c8697af3953227d068c
tomyyn <tomasnaiouf@gmail.com>
2022-03-03 15:46:40 -0300

    Complete excersices

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -7,5 +7,80 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each()
+    for e in self do
+      yield e
+    end
+    self
+  end
+
+  def my_each_with_index
+    i = 0
+    while i < self.length
+      yield self[i],i
+      i+=1
+    end
+    self
+  end
+
+  def my_select
+    ret = []
+    for e in self do
+      ret<<e if(yield e)
+    end
+    ret
+  end
+
+  def my_all?
+    return unless block_given?
+    for e in self do
+      return false unless yield e
+    end
+    return true
+  end
+
+  def my_any?
+    return unless block_given?
+    for e in self do
+      return true if yield e
+    end
+    return false
+  end
+
+  def my_none?
+    return unless block_given?
+    for e in self do
+      return false if yield e
+    end
+    return true
+  end
+
+  def my_count
+    return self.length unless block_given?
+    count=0
+    for e in self do
+      count+=1 if yield e
+    end
+    count
+  end
+
+  def my_map
+    return self unless block_given?
+    res=[]
+    for e in self do
+      res << (yield e)
+    end
+    res
+  end
+
+  def my_inject(initial_value)
+    val=initial_value
+    return val unless block_given?
+
+    for e in self do
+      val = (yield val,e)
+    end
+    val
+  end
+    
 end
